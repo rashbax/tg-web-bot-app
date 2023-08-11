e77a8ef489a54848022a3967bacec64f8e6765b0
Bahodir <rbaxa.br@gmail.com>
2023-08-11 16:45:33 +0500

    'last'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './Styles/Global.css'
-import {  HashRouter } from 'react-router-dom';
+import {  BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Store from './app/store/Store';
 
@@ -11,10 +11,10 @@ import Store from './app/store/Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={Store}>
-    <HashRouter>
+    <BrowserRouter>
     <App />
 
-  </HashRouter>
+  </BrowserRouter>
   </Provider>
     
   
